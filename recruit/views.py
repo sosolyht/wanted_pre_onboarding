325b460831bbdc5d5c6416e321f666c8f39702bd
sosolyht <sosolyht@gmail.com>
2022-06-14 23:25:34 +0900

    Feat: add job posting
    채용공고 등록 API 기능
    데이터가 입력안되었을시 Keyerror 와 HTTP 상태코드 400 반환

--- recruit/views.py
+++ recruit/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
 
-# Create your views here.
+from .models import Recruit
+
+import json
+
+class RecruitView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            Recruit.objects.create(
+                company_id = data['company_id'],
+                position_id = data['position_id'],
+                compensation = data['compensation'],
+                content = data['content'],
+                stack_id = data['stack_id'] 
+            )
+
+            return JsonResponse({'message' : 'SUCCESS'}, status=201)
+
+        except KeyError :
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
