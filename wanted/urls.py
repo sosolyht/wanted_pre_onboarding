43a86acd0f8119605a2e58a32ed94ce58420bc69
sosolyht <sosolyht@gmail.com>
2022-06-14 21:24:50 +0900

    Fix: urls typo

--- wanted/urls.py
+++ wanted/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('recruit/', include('recurit.urls')),
+    path('recruit/', include('recruit.urls')),
 ]
