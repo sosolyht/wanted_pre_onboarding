52219d190a865d9c3218553e800a05b7c78d3b83
sosolyht <sosolyht@gmail.com>
2022-06-15 11:23:32 +0900

    Feat: add users models
    유저 모델추가
    지원내역 모델추가
    
    마이그레이션 파일 추가

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+from recruit.models import Recruit
+
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=15)
+
+    class Meta:
+        db_table = 'users'
+
+
+class ApplyDetail(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recruit = models.ForeignKey(Recruit, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'apply_details'
